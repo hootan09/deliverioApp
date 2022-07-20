80617fce1c0f2247fb4bf99acd06c70052e61657
niki <hootan09@gmail.com>
2022-07-20 22:34:34 +0430

    working on redux

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { store } from './store'
+import { Provider } from 'react-redux'
+
 import { useFonts } from 'expo-font';
+
 import HomeScreen from './Screens/HomeScreen';
 import ResturantScreen from './Screens/ResturantScreen';
 
@@ -19,15 +23,17 @@ export default function App() {
 
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="HomeScreen" component={HomeScreen} options={{
-        //headerShown: false
-      }} />
-      <Stack.Screen name="ResturantScreen" component={ResturantScreen} options={{
-        headerShown: false,
-        presentation: 'modal'
-      }} />
-    </Stack.Navigator>
+      <Provider store={store}>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{
+          //headerShown: false
+        }} />
+        <Stack.Screen name="ResturantScreen" component={ResturantScreen} options={{
+          headerShown: false,
+          presentation: 'modal'
+        }} />
+      </Stack.Navigator>
+      </Provider>
   </NavigationContainer>
   );
 }
